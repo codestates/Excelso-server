bb6151c1105e7a7a14f2f96eea051f903e218426
SIBChoi <qorwh1224@naver.com>
2021-06-24 22:23:01 +0900

    [DB] Create DB

---
+++ models/user.js
@@ -0,0 +1,30 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Review }) {
+      // define association here
+      this.hasMany(Review, {
+        foreignKey: "user_id",
+      });
+    }
+  }
+  User.init(
+    {
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      nickname: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "User",
+      tableName: "User",
+    }
+  );
+  return User;
+};
